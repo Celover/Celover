28b5e28ec0496015f69b4f71eb4b0d34ee9cd392
YongJinPark91 <skylock45@naver.com>
2023-12-13 14:05:01 +0900

    feat create spring boot projcet
    subject
      - create a new project (spring boot)

---
+++ Celover/src/main/java/com/fan/celover/CeloverApplication.java
@@ -0,0 +1,13 @@
+package com.fan.celover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeloverApplication {
+
+	public static void main(String[] args) {
+		System.out.println("잘나오나?!~!");
+	}
+
+}
---
+++ Celover/src/test/java/com/fan/celover/CeloverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fan.celover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeloverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
